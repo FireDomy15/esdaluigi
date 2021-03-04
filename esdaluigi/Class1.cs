984cbdb69991dc42e274407b196f6bbf71be7489
domenicograsso <domenicograsso@DOMENICOGRAD6C7>
2021-03-04 17:56:02 +0100

    Aggiunte strutture dinamiche di prodotto e acquisto

--- esdaluigi/Class1.cs
+++ esdaluigi/Class1.cs
@@ -12,30 +12,45 @@ namespace esdaluigi
         public string password { get; set; }
         public string nome { get; set; }
         public string cognome { get; set; }
+        public List<Acquisto> carrello { get; set; }
 
-        public User(string username, string password, string nome, string cognome)
+    public User(string username, string password, string nome, string cognome, List<Acquisto> carrello)
         {
             this.username = username;
             this.password = password;
             this.nome = nome;
             this.cognome = cognome;
+            this.carrello = carrello;
         }
         
     }
 
-    class prodotto
+    class Prodotto
     {
-        public string nome;
-        public double prezzo;
-        public string descrizione;
-    
-        public prodotto(string nome, double prezzo, string descrizione)
+        public string nome { get; set; }
+        public double prezzo { get; set; }
+        public string descrizione { get; set; }
+
+        public Prodotto(string nome, double prezzo, string descrizione)
         {
             this.nome = nome;
             this.prezzo = prezzo;
             this.descrizione = descrizione;
         }
     }
+
+    class Acquisto
+    {
+        public int quantita { get; set; }
+        public string nomeProdotto { get; set; }
+
+        public Acquisto(int quantita, string nomeProdotto)
+        {
+            this.quantita = quantita;
+            this.nomeProdotto = nomeProdotto;
+        }
+    }
+
     /*class prodotti
     {
         public static int n;
@@ -50,5 +65,4 @@ namespace esdaluigi
             public string descrizione;
         }
     }*/
-}
 }
